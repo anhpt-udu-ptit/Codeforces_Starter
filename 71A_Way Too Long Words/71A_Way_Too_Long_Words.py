7fe72a48d7b65b17fe14c0d6f6422aa2f1cfd923
Phạm Tuấn Anh <anhpt.udu.ptit@gmail.com>
2025-10-15 23:20:18 +0700

    Remove unnecessary str() conversion from input
    Simplified word input by removing redundant str() conversion, as input() already returns a string.

--- 71A_Way Too Long Words/71A_Way_Too_Long_Words.py
+++ 71A_Way Too Long Words/71A_Way_Too_Long_Words.py
@@ -1,7 +1,7 @@
 a = []
 n = int(input())
 for _ in range(n):
-    word = str(input())        
+    word = input()
     w = list(word)
     if len(w) > 10:
         a.append(f'{w[0]}{len(w)-2}{w[len(w)-1]}')
