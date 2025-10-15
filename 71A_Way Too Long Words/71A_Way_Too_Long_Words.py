20b3fcc31571f339ebfc891198ee61b3b45aa2fa
Phạm Tuấn Anh <anhpt.udu.ptit@gmail.com>
2025-10-15 23:11:39 +0700

    Add solution for Way Too Long Words problem
    Implemented Python script to abbreviate words longer than 10 characters as per problem statement. Added an image file to the problem directory.

---
+++ 71A_Way Too Long Words/71A_Way_Too_Long_Words.py
@@ -0,0 +1,11 @@
+a = []
+n = int(input())
+for _ in range(n):
+    word = str(input())        
+    w = list(word)
+    if len(w) > 10:
+        a.append(f'{w[0]}{len(w)-2}{w[len(w)-1]}')
+    else:
+        a.append(word)
+for i in range(len(a)):
+        print(a[i])
