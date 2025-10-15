3cdfa9de1aeb52b77773a8c2ce91555a3128be67
Phạm Tuấn Anh <anhpt.udu.ptit@gmail.com>
2025-10-15 11:58:26 +0700

    Add files via upload

---
+++ 231A_Team.py
@@ -0,0 +1,12 @@
+#Team
+
+if __name__ == "__main__":
+    so_bai_giai = 0
+    n = int(input())
+    for _ in range(n):
+        x, y ,z=map(int, input().split())
+        if x + y + z >= 2:
+            so_bai_giai += 1
+
+    
+    print(so_bai_giai)
